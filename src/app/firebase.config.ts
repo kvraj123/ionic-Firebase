794fd2e7acaffca9664da7f23a88f72d7a10119c
kvraj123 <poojarykaviraj7@gmail.com>
2021-02-23 21:25:28 +0530

    firstcodepush

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+//config default
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGEINGSENDER_ID
+};
+
